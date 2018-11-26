2595dd53e820d03fa76220742d361619b47759e9
overlordqd <batu1905@gmail.com>
2018-11-27 02:26:20 +0300

    initial

---
+++ calling-rest-apis.ps1
@@ -0,0 +1,28 @@
+﻿$_countryResolveUrl = "https://restcountries.eu/rest/v2/alpha/"
+$_airportResolveUrl = "https://www.air-port-codes.com/api/v1/autocomplete"
+
+Get-Content C:\Users\b.akcay\Documents\country-codes.txt | Where-Object {$_ -ne ""} | ForEach-Object {
+    $countryName = $countryCode
+    $airportName = $airportCode
+    $countryCode = ($_ -split "	")[0]
+    $airportCode = ($_ -split "	")[1]
+    $countryUrl = $_countryResolveUrl + $countryCode
+    try{
+        $airportBody = @{"limit"="1"; "term"= $airportCode; }
+        $restAirportAnswer = Invoke-RestMethod -Method 'Post' -Body $airportBody -Uri $_airportResolveUrl -Headers @{'Content-Type' = 'application/x-www-form-urlencoded'; 'APC-Auth' = '96dc04b3fb'; 'Referer'= 'https://www.air-port-codes.com/'}
+        $airportName = $restAirportAnswer.airports[0].name
+    }
+    catch{
+        
+    }
+
+    try{
+        $restCountryAnswer = Invoke-RestMethod -Method 'Get' -Uri $countryUrl
+        $cityName = $restCountryAnswer.name        
+    }
+    catch{
+        
+    }
+
+    Write-Host $cityName $airportName
+}
---
+++ renamesubtitles.ps1
@@ -0,0 +1,14 @@
+﻿dir | ?{$_.Name.EndsWith('.mkv')} | %{ 
+    $episodeSignature = (Select-String -InputObject $_.BaseName -Pattern 'S\d{1,2}E\d{1,2}' -AllMatches).Matches[0].Value
+    $matchingSubtitle = (dir | ?{ $_.Name.EndsWith('.srt') -and $_.Name -match $episodeSignature })
+
+    if($matchingSubtitle -eq $null)
+    {
+        'cant find the subtitle for ' + $episodeSignature
+    }
+    else
+    {
+        ren $matchingSubtitle[0].Name ($_.BaseName + $matchingSubtitle[0].Extension)
+        'renamed ' + $episodeSignature
+    }
+}
