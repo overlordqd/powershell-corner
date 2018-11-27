e78262618a25843e86f6fabdd979670df46d7c37
overlordqd <batu1905@gmail.com>
2018-11-27 13:40:51 +0300

    readme

--- renamesubtitles.ps1
+++ renamesubtitles.ps1
@@ -1,4 +1,16 @@
-﻿dir | ?{$_.Name.EndsWith('.mkv')} | %{ 
+# This piece of code renames all subtitle files in the executing directory to match the file name of corresponding mkv file.
+# Almost all the smart TVs refuse to open the subtitle file unless the base video file name is exactly the same.
+# This powershell script is better than renaming all the files yourself, one by one.
+#
+# Before:
+# X.S02E01.x264.mkv
+# subtitle02e01.srt
+#
+# After:
+# X.S02E01.x264.mkv
+# X.S02E01.x264.srt
+
+dir | ?{$_.Name.EndsWith('.mkv')} | %{ 
     $episodeSignature = (Select-String -InputObject $_.BaseName -Pattern 'S\d{1,2}E\d{1,2}' -AllMatches).Matches[0].Value
     $matchingSubtitle = (dir | ?{ $_.Name.EndsWith('.srt') -and $_.Name -match $episodeSignature })
 
